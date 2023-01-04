6a62e8f19623cef0dfa2e37ab8c5d3cdbb4d84de
tursynbekoff <talgat.tursynbekov@gmail.com>
2023-01-04 12:08:53 +0100

    feat:adding aliases, navbar and configuration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
+import Navbar from 'components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,6 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
+        <Navbar />
         <h1>
           Some text for test
         </h1>
