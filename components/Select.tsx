57a7d4941673ee51dfa7b2226ceee14af425758e
tursynbekoff <talgat.tursynbekov@gmail.com>
2023-02-01 17:16:59 +0100

    fix: centering text

--- components/Select.tsx
+++ components/Select.tsx
@@ -11,13 +11,13 @@ const Select = () => {
       <ul className="inline-flex items-center relative h-10 p-1 space-x-1 bg-gray-200 rounded-md font-semibold text-blue-600 ">
         <li className="relative w-full">
           <input className="sr-only peer" type="radio" value="thin" name="dough" id={doughList[0]} />
-          <label className="flex w-full py-1 px-2 bg-white border border-gray-300 rounded-lg cursor-pointer focus:outline-none hover:bg-gray-50 peer-checked:ring-teal-500 peer-checked:ring-2 peer-checked:border-transparent" htmlFor={doughList[0]}>
+          <label className="flex justify-center w-full py-1 px-2 bg-white border border-gray-300 rounded-lg cursor-pointer focus:outline-none hover:bg-gray-50 peer-checked:ring-teal-500 peer-checked:ring-2 peer-checked:border-transparent" htmlFor={doughList[0]}>
             Thin
           </label>
         </li>
         <li className="relative w-full">
           <input className="sr-only peer" type="radio" value="classic" name="dough" id={doughList[1]} />
-          <label className="flex w-full py-1 px-2 bg-white border border-gray-300 rounded-lg cursor-pointer focus:outline-none hover:bg-gray-50 peer-checked:ring-violet-500 peer-checked:ring-2 peer-checked:border-transparent" htmlFor={doughList[1]}>
+          <label className="flex justify-center w-full py-1 px-2 bg-white border border-gray-300 rounded-lg cursor-pointer focus:outline-none hover:bg-gray-50 peer-checked:ring-violet-500 peer-checked:ring-2 peer-checked:border-transparent" htmlFor={doughList[1]}>
             Classic
           </label>
         </li>
@@ -25,19 +25,19 @@ const Select = () => {
       <ul className="inline-flex items-center relative h-10 p-1 space-x-1 bg-gray-200 rounded-md font-semibold text-blue-600 ">
         <li className="relative w-full">
           <input className="sr-only peer" type="radio" value={26} name="size" id={idList[0]} />
-          <label className="flex w-full py-1 px-2 bg-white border border-gray-300 rounded-lg cursor-pointer focus:outline-none hover:bg-gray-50 peer-checked:ring-green-500 peer-checked:ring-2 peer-checked:border-transparent" htmlFor={idList[0]}>
+          <label className="flex justify-center w-full py-1 px-2 bg-white border border-gray-300 rounded-lg cursor-pointer focus:outline-none hover:bg-gray-50 peer-checked:ring-green-500 peer-checked:ring-2 peer-checked:border-transparent" htmlFor={idList[0]}>
             26 cm
           </label>
         </li>
         <li className="relative w-full">
           <input className="sr-only peer" type="radio" value={30} name="size" id={idList[1]} />
-          <label className="flex w-full py-1 px-2 bg-white border border-gray-300 rounded-lg cursor-pointer focus:outline-none hover:bg-gray-50 peer-checked:ring-red-500 peer-checked:ring-2 peer-checked:border-transparent" htmlFor={idList[1]}>
+          <label className="flex justify-center w-full py-1 px-2 bg-white border border-gray-300 rounded-lg cursor-pointer focus:outline-none hover:bg-gray-50 peer-checked:ring-red-500 peer-checked:ring-2 peer-checked:border-transparent" htmlFor={idList[1]}>
             30 cm
           </label>
         </li>
         <li className="relative w-full">
           <input className="sr-only peer" type="radio" value={34} name="size" id={idList[2]} />
-          <label className="flex w-full py-1 px-2 bg-white border border-gray-300 rounded-lg cursor-pointer focus:outline-none hover:bg-gray-50 peer-checked:ring-yellow-500 peer-checked:ring-2 peer-checked:border-transparent" htmlFor={idList[2]}>
+          <label className="flex justify-center w-full py-1 px-2 bg-white border border-gray-300 rounded-lg cursor-pointer focus:outline-none hover:bg-gray-50 peer-checked:ring-yellow-500 peer-checked:ring-2 peer-checked:border-transparent" htmlFor={idList[2]}>
             34 cm
           </label>
         </li>
