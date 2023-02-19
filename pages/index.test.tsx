befe85b888f1dd96d5395bef477b347ede024af2
tursynbekoff <talgat.tursynbekov@gmail.com>
2023-02-19 16:27:59 +0100

    test: first try

--- pages/index.test.tsx
+++ pages/index.test.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { renderWithProviders } from '../utils/utils-for-tests'
 
 import Home from '.'
 
-test('renders learn react link', () => {
+test('purchase of initial first pizza and comparing to the card counter', async() => {
   renderWithProviders(<Home />);
+
+  const expectedOutput = 1;
+
+  const addButton = await screen.queryAllByText("+ Add")[0];
+  
+  userEvent.click(addButton);
+
+  await screen.debug()
+  
+  // await waitFor(() => {
+  //   expect(screen.getByText('1')).toBeInTheDocument();
+  // });
+
 });
