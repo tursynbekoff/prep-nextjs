707075bbaf23eff4dafe6fb2bca0bb7912acfebd
tursynbekoff <talgat.tursynbekov@gmail.com>
2023-03-15 16:26:58 +0100

    test: all test in categories

--- cypress/e2e/pizza-categories.cy.ts
+++ cypress/e2e/pizza-categories.cy.ts
@@ -8,4 +8,18 @@ describe('Select pizza by categories', () => {
     cy.get('[role="vegetarian"]').click()
     cy.get('[role="pizza-card"]').should('have.length', expectedAmount)
   })
+  it('should in show all pizzas with meat', () => {
+    cy.visit('/')
+    const expectedAmount = 9;
+
+    cy.get('[role="meat"]').click()
+    cy.get('[role="pizza-card"]').should('have.length', expectedAmount)
+  })
+  it('should in show all spicy pizzas', () => {
+    cy.visit('/')
+    const expectedAmount = 9;
+
+    cy.get('[role="spicy"]').click()
+    cy.get('[role="pizza-card"]').should('have.length', expectedAmount)
+  })
 })
